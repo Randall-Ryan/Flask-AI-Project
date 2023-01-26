49ccc0c6fcfb092de702768796dc4040250fe26d
Randall-Ryan <randyryan@Randys-PC.localdomain>
2023-01-25 21:22:01 -0500

    changes to league feature

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 import base64
 import io
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Note, User
 import json
@@ -145,76 +145,97 @@ def delete_note():
             return jsonify({})
 
 
+@views.route("/match/", defaults={"id": 1})
+@views.route("/match/<string:summoner>", methods=["GET", "POST"])
+@login_required
+def match(summoner):
+    # past number of games?
+    # TODO: make better check/error handling if summoner name exists
+
+    my_region = "na1"
+    summoner = lol_watcher.summoner.by_name(my_region, summoner)
+
+    try:
+        response = lol_watcher.match.matchlist_by_puuid(my_region, summoner["puuid"])
+    except ApiError as err:
+        if err.response.status_code == 429:
+            print("We should retry in {} seconds.".format(err.headers["Retry-After"]))
+            print("this retry-after is handled by default by the RiotWatcher library")
+            print("future requests wait until the retry-after time passes")
+        elif err.response.status_code == 404:
+            print("Summoner with that ridiculous name not found.")
+        else:
+            raise
+
+    res = lol_watcher.match.by_id(my_region, response[0])
+    game_info = {}
+    minutes = int(res["info"]["gameDuration"] / 60)
+    seconds = res["info"]["gameDuration"] % 60
+    game_info["gameDuration"] = f"{minutes}:{seconds}"
+    game_info["gameMode"] = res["info"]["gameMode"]
+    game_info["gameId"] = res["info"]["gameId"]
+    participants = res["info"]["participants"]
+
+    averages = {}
+    total_gold = 0
+    total_damage = 0
+    for participant in participants:
+        total_gold = int(participant["goldEarned"] + total_gold)
+        total_damage = int(participant["totalDamageDealt"] + total_damage)
+
+    averages["gold"] = total_gold / len(participants)
+    averages["total_damage"] = total_damage / len(participants)
+
+    for participant in participants:
+        participant["plot_data"] = create_plot_for_participant(participant, averages)
+    return render_template(
+        "match.html",
+        participants=participants,
+        game_info=game_info,
+        user=current_user,
+        summoner=summoner["name"],
+    )
+
+
 @views.route("/league-form", methods=["GET", "POST"])
 @login_required
 def league_form():
     if request.method == "POST":
         summonerName = request.form.get("summoner")
-        # past number of games?
-        # TODO: make better check/error handling if summoner name exists
         if not summonerName:
             flash("Please enter a valid summoner", category="error")
         else:
-
-            my_region = "na1"
-            summoner = lol_watcher.summoner.by_name(my_region, summonerName)
-
-            try:
-                response = lol_watcher.match.matchlist_by_puuid(
-                    my_region, summoner["puuid"]
-                )
-            except ApiError as err:
-                if err.response.status_code == 429:
-                    print(
-                        "We should retry in {} seconds.".format(
-                            err.headers["Retry-After"]
-                        )
-                    )
-                    print(
-                        "this retry-after is handled by default by the RiotWatcher library"
-                    )
-                    print("future requests wait until the retry-after time passes")
-                elif err.response.status_code == 404:
-                    print("Summoner with that ridiculous name not found.")
-                else:
-                    raise
-
-            res = lol_watcher.match.by_id(my_region, response[0])
-            game_info = {}
-            minutes = int(res["info"]["gameDuration"] / 60)
-            seconds = res["info"]["gameDuration"] % 60
-            game_info["gameDuration"] = f"{minutes}:{seconds}"
-            game_info["gameMode"] = res["info"]["gameMode"]
-            game_info["gameId"] = res["info"]["gameId"]
-            participants = res["info"]["participants"]
-
-            total_gold = 0
-            for participant in participants:
-                total_gold = int(participant["goldEarned"] + total_gold)
-
-            avg_gold = total_gold / len(participants)
-            for participant in participants:
-                plot.close()
-                x = ["Average gold earned", participant["summonerName"]]
-                goldearned = [avg_gold, participant["goldEarned"]]
-                plot.bar(x, goldearned)
-
-                for idx, value in enumerate(goldearned):
-                    plot.text(idx, value, str(int(value)))
-
-                img = io.BytesIO()
-                plot.savefig(img, format="png")
-                img.seek(0)
-                participant["plot_data"] = urllib.parse.quote(
-                    base64.b64encode(img.getvalue()).decode("utf-8")
-                )
-
-            return render_template(
-                "match.html",
-                participants=participants,
-                game_info=game_info,
-                user=current_user,
-                summoner=summonerName,
-            )
+            return redirect(url_for("views.match", summoner=summonerName))
 
     return render_template("league_form.html", user=current_user)
+
+
+def create_plot_for_participant(participant, averages):
+    # TODO: make a better plot with matplotlib, bokeh, or seaborn
+    plot.close()
+    x = [
+        "avg gold",
+        participant["summonerName"] + " gold",
+        "avg damage",
+        participant["summonerName"] + " damage",
+    ]
+    data = [
+        averages["gold"],
+        participant["goldEarned"],
+        averages["total_damage"],
+        participant["totalDamageDealt"],
+    ]
+    plot.bar(x, data)
+
+    for idx, value in enumerate(data):
+        plot.text(idx, value + 100, str(int(value)))
+
+    img = io.BytesIO()
+    plot.savefig(img, format="png")
+    img.seek(0)
+    participant["plot_data"] = urllib.parse.quote(
+        base64.b64encode(img.getvalue()).decode("utf-8")
+    )
+    plot_data = urllib.parse.quote(base64.b64encode(img.getvalue()).decode("utf-8"))
+
+    return plot_data
